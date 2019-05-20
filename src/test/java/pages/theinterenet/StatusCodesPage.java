5f3b763fd756fadbe9d3d63eabec8a753c2bb540
A698349 <A698349@MC0YUD5C.ww930.my-it-solutions.net>
2019-05-20 20:45:14 +0200

    added expected exception test for code 301 - clean up

--- src/test/java/pages/theinterenet/StatusCodesPage.java
+++ src/test/java/pages/theinterenet/StatusCodesPage.java
@@ -2,7 +2,6 @@ package pages.theinterenet;
 
 import io.restassured.RestAssured;
 
-//import org.apache.http.client.ClientProtocolException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
